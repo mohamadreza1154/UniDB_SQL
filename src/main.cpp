19fdaba76f3d7ca0f00cce320c6712ff403a7a2f
مجمدرضا خواجوئی <mohamadrezakhajoie@gmail.com>
2025-12-24 23:25:54 +0330

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #include "storage/LinkedListCollection.h"
 #include "engine/ExecutionEngine.h"
 #include "parser/QueryParser.h"
-
+using namespace std;
 int main() {
     ArrayCollection arrayStudents;
     LinkedListCollection listStudents;
@@ -17,12 +17,12 @@ int main() {
 
     QueryParser parser;
 
-    std::cout << "UniDB shell. Type 'exit' to quit.\n";
+    cout << "UniDB shell. Type 'exit' to quit.\n";
 
-    std::string line;
+    string line;
     while (true) {
-        std::cout << "> ";
-        if (!std::getline(std::cin, line)) {
+        cout << "> ";
+        if (!getline(cin, line)) {
             break;
         }
         if (line == "exit" || line == "quit") {
@@ -34,7 +34,7 @@ int main() {
 
         Command cmd = parser.parse(line);
         if (cmd.type == CommandType::Invalid) {
-            std::cout << "Invalid command.\n";
+            cout << "Invalid command.\n";
             continue;
         }
 
@@ -45,24 +45,24 @@ int main() {
         ExecutionResult res = engine.execute(cmd);
 
         if (!res.ok) {
-            std::cout << "Error: " << res.message << "\n";
+            cout << "Error: " << res.message << "\n";
             continue;
         }
 
         if (res.hasStudent) {
-            std::cout << "id=" << res.student.id
+            cout << "id=" << res.student.id
                       << " name=" << res.student.name
                       << " gpa=" << res.student.gpa << "\n";
         } else if (res.hasStudents) {
             for (const auto& s : res.students) {
-                std::cout << "id=" << s.id
+                cout << "id=" << s.id
                           << " name=" << s.name
                           << " gpa=" << s.gpa << "\n";
             }
         } else if (res.hasNumber) {
-            std::cout << res.number << "\n";
+            cout << res.number << "\n";
         } else {
-            std::cout << res.message << "\n";
+            cout << res.message << "\n";
         }
     }
 
